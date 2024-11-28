920181ab047ea4cecedb9161e310c37b8ec63cc5
OfflineWork <sovaelias@gmail.com>
2024-11-28 21:20:00 +0200

    Document HomeController

--- MovieApp/Controllers/HomeController.cs
+++ MovieApp/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace MovieApp.Controllers
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly IConfiguration _configuration;
 
+        // Constructor - pass services to methods
         public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, IHttpClientFactory httpClient, IConfiguration configuration)
         {
             _logger = logger;
@@ -24,12 +25,17 @@ namespace MovieApp.Controllers
             _configuration = configuration;
         }
 
+        // Return index and pass all movies in the database as list
         public IActionResult Index()
         {
             var allMovies = _context.Movies.ToList();
             return View(allMovies);
         }
 
+
+        // Authorized page - return only for users with the role "StoreManager".
+        // Return the page to submit a new movie or edit an existing one. If an ID is passed along to the page (meaning we are editing a page)
+        // Then find the movie with that ID in the database, else initialize a new movie object to pass to the Form method.
         [Authorize(Roles = "StoreManager")]
         public IActionResult CreateEditMovie(int? id)
         {
@@ -40,12 +46,15 @@ namespace MovieApp.Controllers
             return View(movie);
         }
 
+        // Same as index. Parse movies from the database into a list and pass them to the page
         public IActionResult ManagerOverview()
         {
             var allMovies = _context.Movies.ToList();
             return View(allMovies);
         }
 
+        // First find the movie in the database. Then find the relevant information returned by the API to that movie after it was created. Parse both movie models into
+        // the view model and return it along with the page.
         public IActionResult MovieDetails(int id, string name)
         {
             var movieController = new MovieController(_Movielogger, _context, _httpClientFactory, _configuration);
@@ -73,6 +82,8 @@ namespace MovieApp.Controllers
             return View(viewModel);
         }
 
+
+        // Error page
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
